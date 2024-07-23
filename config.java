99b038d66fb020190377a66a34860a0011016545
Ghazala Babar <ghazala.babar@emumba.com>
2024-07-23 17:16:19 +0500

    Create config.java

---
+++ config.java
@@ -0,0 +1,5 @@
+ "browser": "chrome",
+  "gridUrl": "http://localhost:4444/wd/hub",
+  "url": "https://www.saucedemo.com",
+  "chromedriver.path": "C:\\Users\\Admin\\IdeaProjects\\Junitcourse\\resources\\chromedriver.exe",
+  "geckodriver.path": "C:\\Users\\Admin\\IdeaProjects\\Junitcourse\\resources\\geckodriver.exe"
